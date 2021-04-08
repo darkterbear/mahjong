652fba2246ae3cf3e813a3a4b3b906845c2e8693
Terrance Li <terranceli.terbear@gmail.com>
2021-04-08 16:55:17 -0700

    Added start game handler

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,6 +2,7 @@ import { Application } from 'express';
 import { Server } from 'socket.io';
 import Player from '@game/Player';
 import Room from '@game/Room';
+import Tile from '@game/Tile';
 
 export default function routes(app: Application, io: Server): void {
   app.post('/create_room', (req, res) => {
@@ -38,6 +39,32 @@ export default function routes(app: Application, io: Server): void {
     io.to(room.code).emit('update_players', room.playerNames(), room.leader?.username);
 
     res.json({ players: room.playerNames(), leader: room.leader?.username });
+  });
+
+  app.post('/start_game', (req, res) => {
+    const id = req.sessionID;
+    const player = Player.getPlayer(id);
+    const room = player?.room;
+
+    if (!room || room.players.length < 4) {
+      return res.status(400).end();
+    }
+
+    // Update game state
+    room.deck = Tile.shuffledDeck();
+    
+    // Deal hands
+    for (const p of room.players) {
+      p.handConcealed = room.deck.splice(0, 13);
+    }
+
+    room.turn = 0;
+    room.players[0].handConcealed.push(room.deck.pop() as Tile);
+
+    res.status(200).end();
+
+    // Send start game
+    io.to(room.code).emit('start_game');
   });
   
 }
