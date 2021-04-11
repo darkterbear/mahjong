3aa6fc4d12980945c11476a25aa16eae1311e4c0
Terrance Li <terranceli.terbear@gmail.com>
2021-04-11 09:58:19 -0700

    Styling textbox and buttons

--- client/src/pages/MenuPage.js
+++ client/src/pages/MenuPage.js
@@ -4,5 +4,8 @@ import './MenuPage.scss'
 export function MenuPage() {
   return <div id="page">
     <h1>麻将</h1>
+    <input placeholder="Username"/>
+    <button>Create Room</button>
+    <button>Join Room</button>
   </div>
 }
